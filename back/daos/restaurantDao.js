8357a88b064707ceeecf28c7e871531102b172ce
PetraSilavuori <petra.silavuori@alten.fi>
2020-01-22 14:44:12 +0200

    Routes for adding a restaurant and getting all works.

--- back/daos/restaurantDao.js
+++ back/daos/restaurantDao.js
@@ -1,10 +1,17 @@
 const Restaurant = require('../models/restaurantModel');
 
+const daoAddRestaurant = async (restaurant) => {
+  const newRestaurant = new Restaurant(restaurant);
+  const savedRestaurant = await newRestaurant.save();
+  return savedRestaurant;
+};
+
 const daoGetAllRestaurants = async () => {
   const restaurants = await Restaurant.find({});
   return restaurants;
 };
 
 module.exports = {
+  daoAddRestaurant,
   daoGetAllRestaurants
 };
--- back/routes/restaurantRoutes.js
+++ back/routes/restaurantRoutes.js
@@ -1,9 +1,23 @@
 const router = require('express').Router();
 const {
+  daoAddRestaurant,
   daoGetAllRestaurants
 } = require('../daos/restaurantDao');
 
 module.exports = router;
+
+const addRestaurant = async (req, res, next) => {
+  const { body } = req;
+  try {
+    const sentRestaurant = await daoAddRestaurant(
+      body,
+    );
+    return res.send(sentRestaurant);
+  } catch (err) {
+    return next(res.send({ message: err.toString() 
+    }));
+  };
+};
 
 const getAllRestaurants = async (req, res, next) => {
   try {
@@ -17,3 +31,4 @@ const getAllRestaurants = async (req, res, next) => {
 };
 
 router.get('/getAllRestaurants', getAllRestaurants);
+router.post('/addRestaurant', addRestaurant);
